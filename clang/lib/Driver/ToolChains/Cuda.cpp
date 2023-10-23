47b9b937feee50f3aec5de2e12ea42053498818b
Qi Hangkong <qihangkong@outlook.com>
2023-10-23 12:28:27 +0800

    feat: add a header file __clang_cuda_runtime_rvgpu_wrapper.h to replace __clang_cuda_runtime_wrapper.h

--- clang/lib/Driver/ToolChains/Cuda.cpp
+++ clang/lib/Driver/ToolChains/Cuda.cpp
@@ -300,7 +300,7 @@ void CudaInstallationDetector::AddCudaIncludeArgs(
   }
 
   CC1Args.push_back("-include");
-  CC1Args.push_back("__clang_cuda_runtime_wrapper.h");
+  CC1Args.push_back("__clang_cuda_runtime_rvgpu_wrapper.h");
 }
 
 void CudaInstallationDetector::CheckCudaVersionSupportsArch(
